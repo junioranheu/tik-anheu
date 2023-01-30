68ebc67f3a1df92648974e069d4263cbf59c60a8
junioranheu <junioranheu@gmail.com>
2023-01-30 15:09:21 -0300

    continuar dev video.opcoes

--- src/components/video/video.opcoes.tsx
+++ src/components/video/video.opcoes.tsx
@@ -1,4 +1,9 @@
+import ImgComentario from '@/assets/images/icones/comentario.webp';
+import ImgCompartilhar from '@/assets/images/icones/compartilhar.webp';
+import ImgCoracao from '@/assets/images/icones/coracao.webp';
 import Styles from '@/components/video/styles/video.opcoes.module.scss';
+import useWindowSize from '@/hooks/outros/useWindowSize';
+import Image, { StaticImageData } from 'next/image';
 
 interface iParametros {
     id: string;
@@ -6,11 +11,36 @@ interface iParametros {
 }
 
 export default function VideoOpcoes({ id, videoWidth }: iParametros) {
+
+    const tamanhoTela = useWindowSize();
+
     return (
-        <div className={Styles.opcoes}>
+        <div
+            className={Styles.opcoes}
+            style={{ marginLeft: tamanhoTela?.width! > 801 ? `${(videoWidth + 125)}px` : '' }}
+        >
             <span>#{id}</span>
-            <span>aea pues</span>
-            <span>{videoWidth}</span>
+
+            <Icone imagem={ImgCoracao} title='Curtir vídeo' isVermelho={true} />
+            <Icone imagem={ImgComentario} title='Comentários' isVermelho={false} />
+            <Icone imagem={ImgCompartilhar} title='Compartilhar' isVermelho={false} />
+        </div>
+    )
+}
+
+interface iIcone {
+    imagem: StaticImageData;
+    title: string;
+    isVermelho: boolean;
+}
+
+export function Icone({ imagem, title, isVermelho }: iIcone) {
+    return (
+        <div
+            className={`${Styles.icone} ${(isVermelho && Styles.iconeVermelhoHover)}`}
+            title={title}
+        >
+            <Image src={imagem} alt='' />
         </div>
     )
 }
